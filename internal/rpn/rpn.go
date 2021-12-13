fa77e8ff5a5b7e2a8d17b72beabeb16e8e4ed54a
Serg_Kotovsky <rookulab@mail.ru>
2021-12-13 23:44:17 +0300

    fix functions in rpn.go

--- internal/rpn/rpn.go
+++ internal/rpn/rpn.go
@@ -2,31 +2,31 @@ package rpn
 
 import "github.com/seggga/he/internal/domain"
 
-func ConvertToRPN(query []domain.Token) []domain.Token {
+func ConvertToRPN(conditionTokens []domain.Token) []domain.Token {
 
 	var rpn, stack []domain.Token
-	var stackPriority, lexPriority int
+	// var stackPriority, tokenPriority int
 
-	for _, lex := range query {
+	for _, token := range conditionTokens {
 		// операнд сразу заносится в выходную строку
-		if isOperand(lex) {
-			rpn = append(rpn, lex)
+		if isOperand(token) {
+			rpn = append(rpn, token)
 			continue
 		}
 
 		// открывающая скобка сразу идет в стек
-		if lex.Lexema == "(" {
-			stack = append(stack, lex)
+		if string(token.Lexema) == "(" {
+			stack = append(stack, token)
 			continue
 		}
 
 		// закрывающая скобка извлекает все элементы из стека до символа "("
-		if lex.Lexema == ")" {
+		if string(token.Lexema) == ")" {
 			for i := len(stack) - 1; i >= 0; i -= 1 {
 				stackLex := stack[i]
 				stack = stack[:i]
 
-				if stackLex.Lexema == "(" {
+				if string(stackLex.Lexema) == "(" {
 					break
 				}
 
@@ -36,12 +36,12 @@ func ConvertToRPN(query []domain.Token) []domain.Token {
 		}
 
 		// оператор участвует в ветвлении
-		if isOperator(lex) {
-			lexPriority = getPriority(lex)
+		if isOperator(token) {
+			// tokenPriority = getPriority(lex)
 
 			// стек пуст - записываем знак операции в стек
 			if len(stack) == 0 {
-				stack = append(stack, lex)
+				stack = append(stack, token)
 				continue
 			}
 
@@ -49,23 +49,23 @@ func ConvertToRPN(query []domain.Token) []domain.Token {
 			// затем помещаем токен в стек
 			for i := len(stack) - 1; i >= 0; i -= 1 {
 
-				stackLex := stack[i]
-				stackPriority = getPriority(stackLex)
+				stackToken := stack[i]
+				// stackPriority = stack[i].Priority
 
 				// приоритет у токена больше, чем у вершины стека - кладем токен в стек
-				if lexPriority > stackPriority {
-					stack = append(stack, lex)
+				if token.Priority > stackToken.Priority {
+					stack = append(stack, token)
 					break
 				}
 
 				stack = stack[:i]
 
-				rpn = append(rpn, stackLex)
+				rpn = append(rpn, stackToken)
 			}
 
 			// если из стека извлекли все операторы, то токен заносим в стек
 			if len(stack) == 0 {
-				stack = append(stack, lex)
+				stack = append(stack, token)
 			}
 
 		} // if isOperator(token)
@@ -81,44 +81,16 @@ func ConvertToRPN(query []domain.Token) []domain.Token {
 	return rpn
 }
 
-func isOperator(lex token.Lexema) bool {
-
-	switch lex.Token {
-	case "COMP":
+func isOperator(token domain.Token) bool {
+	if token.TokenType == "operator" {
 		return true
-	case "AND", "OR":
-		return true
-	default:
-		return false
-
 	}
+	return false
 }
 
-func isOperand(lex token.Lexema) bool {
-
-	if isOperator(lex) {
-		return false
-	}
-
-	switch lex.Token {
-	case "PAREN":
-		return false
-	default:
+func isOperand(token domain.Token) bool {
+	if token.TokenType == "column name" || token.TokenType == "string" || token.TokenType == "integral" {
 		return true
 	}
-}
-
-func getPriority(lex token.Lexema) int {
-	switch lex.Litera {
-	case "(":
-		return 0
-	case ")":
-		return 1
-	case "AND", "OR":
-		return 2
-	case ">", "<", "==", ">=", "<=", "=":
-		return 3
-	default:
-		return 100 // never gonna be returned
-	}
+	return false
 }
