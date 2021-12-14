561126dab03164fbb5f1d0cb700e574a4208ff60
Serg_Kotovsky <rookulab@mail.ru>
2021-12-15 00:03:51 +0300

    main logic in queryservice.go

--- internal/services/queryservice.go
+++ internal/services/queryservice.go
@@ -1,17 +1,72 @@
 package services
 
-import "github.com/seggga/he/internal/domain"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/seggga/he/internal/domain"
+)
 
 type queryService struct {
-	query  domain.QueryReader
-	parser domain.QueryParser
-	csv    domain.CSVFileReader
+	query   domain.QueryReader
+	parser  domain.QueryParser
+	checker domain.ConditionChecker
+	csv     domain.CSVFileReader
+	sep     string
 }
 
 func (qs *queryService) Run() {
-
 	// read query
+	sql, err := qs.query.Read()
+	if err != nil {
+		// log
+		fmt.Println("program exit")
+	}
 	// parse query
+	err = qs.parser.Parse(sql)
+	if err != nil {
+		// log
+		fmt.Println("program exit")
+	}
+	// variable to check, if head has already been printed
+	headNotPrinted := false
+	for _, v := range qs.parser.GetFiles() {
+		// initialize csv-reader
+		err := qs.csv.Init(v)
+		if err != nil {
+			// log
+			fmt.Println("Program exit")
+		}
+		// read head of the csv-file
+		head, err := qs.csv.Head()
+		if err != nil {
+			// log cannot read csv-head
+			continue
+		}
+		if headNotPrinted {
+			printData(qs.parser.GetSelect(), head, head, qs.sep)
+			headNotPrinted = true
+		}
+		// read csv-rows
+		for {
+			row, err := qs.csv.Row()
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					// log debug reached the end of file
+					break
+				}
+				// log cannot read csv-row
+				break
+			}
+			// check condition
+			if qs.checker.Check(head, row) {
+				printData(qs.parser.GetSelect(), head, row, qs.sep)
+			}
+		}
+
+	}
 	// get rpn
 	/* for files from query {
 
@@ -28,10 +83,26 @@ func (qs *queryService) Run() {
 	*/
 }
 
-func NewService(q domain.QueryReader, p domain.QueryParser, csv domain.CSVFileReader) queryService {
+func NewService(q domain.QueryReader, p domain.QueryParser, checker domain.ConditionChecker, csv domain.CSVFileReader, sep string) queryService {
 	return queryService{
-		query:  q,
-		parser: p,
-		csv:    csv,
+		query:   q,
+		parser:  p,
+		checker: checker,
+		csv:     csv,
+		sep:     sep,
 	}
+}
+
+func printData(sel []string, head []string, row []string, sep string) {
+	for i, v := range sel {
+		for j, w := range head {
+			if strings.ToLower(v) == strings.ToLower(w) {
+				fmt.Printf("%s", row[j])
+			}
+		}
+		if i < len(sel)-1 {
+			fmt.Print("%s", sep)
+		}
+	}
+	fmt.Printf("\n")
 }
