bc2c39a40fdf4107646946eb467af40a7a235fdd
Serg_Kotovsky <rookulab@mail.ru>
2021-12-18 18:45:52 +0300

    add context and signal in main.go

--- cmd/sqlparser/main.go
+++ cmd/sqlparser/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/seggga/he/internal/pkg/config"
 	"github.com/seggga/he/internal/pkg/csvfile"
@@ -28,9 +32,22 @@ func main() {
 	checker := new(rpn.ConditionCheck)
 	csv := new(csvfile.CSVScanner)
 
-	service := services.NewService(query, &parser, checker, csv, cfg.Separator)
-	service.Run()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(cfg.Timeout)*time.Second)
+	service := services.NewService(query, &parser, checker, csv, cfg.Separator, ctx)
+	go service.Run()
 
+	sigInt := make(chan os.Signal)
+	signal.Notify(sigInt, syscall.SIGINT)
+
+	select {
+	case <-ctx.Done(): // conctext has been closed due to timeout
+		// log.Println("got INTERRUPT signal")
+	case <-sigInt: // got INT signal
+		// log.Println("got INTERRUPT signal")
+		cancel()
+	}
+
+	fmt.Println("Program exit")
 }
 
 func printAppData() {
