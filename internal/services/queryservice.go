45f965a1e158a76e8abae6ae7189dabf028eff37
Serg_Kotovsky <rookulab@mail.ru>
2021-12-18 18:46:14 +0300

    add context listening in service

--- internal/services/queryservice.go
+++ internal/services/queryservice.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
@@ -15,15 +16,17 @@ type queryService struct {
 	checker domain.ConditionChecker
 	csv     domain.CSVFileReader
 	sep     string
+	ctx     context.Context
 }
 
-func NewService(q domain.QueryReader, p domain.QueryParser, checker domain.ConditionChecker, csv domain.CSVFileReader, sep string) queryService {
+func NewService(q domain.QueryReader, p domain.QueryParser, checker domain.ConditionChecker, csv domain.CSVFileReader, sep string, ctx context.Context) queryService {
 	return queryService{
 		query:   q,
 		parser:  p,
 		checker: checker,
 		csv:     csv,
 		sep:     sep,
+		ctx:     ctx,
 	}
 }
 
@@ -49,45 +52,62 @@ func (qs *queryService) Run() {
 	// if head has already been printed - don't do this again
 	headNotPrinted := true
 	for _, v := range qs.parser.GetFiles() {
-		// initialize csv-reader
-		err := qs.csv.Init(v)
-		if err != nil {
-			// log
-			fmt.Println("error reading file, program exit")
+		select {
+		case <-qs.ctx.Done():
+			qs.csv.Close()
 			return
-		}
-		// read head of the csv-file
-		head, err := qs.csv.Head()
-		if err != nil {
-			// log cannot read csv-head
-			continue
-		}
-		// if head has already been printed - don't do this again
-		if headNotPrinted {
-			printData(qs.parser.GetSelect(), head, head, qs.sep)
-			headNotPrinted = false
-		}
-		// read csv-rows
-		for {
-			row, err := qs.csv.Row()
+		default:
+			// initialize csv-reader
+			err := qs.csv.Init(v)
 			if err != nil {
-				if errors.Is(err, io.EOF) {
-					// log debug reached the end of file
-					break
-				}
-				// log cannot read csv-row
-				break
-			}
-			// fmt.Printf("%v", row)
-			// check condition
-			result, err := qs.checker.Check(head, row)
-			if err != nil {
-				fmt.Printf("error checking condition, program exit %s", err)
-				qs.csv.Close()
+				// log
+				fmt.Println("error reading file, program exit")
 				return
 			}
-			if result {
-				printData(qs.parser.GetSelect(), head, row, qs.sep)
+			// read head of the csv-file
+			head, err := qs.csv.Head()
+			if err != nil {
+				// log cannot read csv-head
+				continue
+			}
+			// if head has already been printed - don't do this again
+			if headNotPrinted {
+				printData(qs.parser.GetSelect(), head, head, qs.sep)
+				headNotPrinted = false
+			}
+			// read csv-rows
+			readRow := true
+			for readRow {
+
+				select {
+				case <-qs.ctx.Done():
+					qs.csv.Close()
+					return
+				default:
+					row, err := qs.csv.Row()
+					if err != nil {
+						if errors.Is(err, io.EOF) {
+							// log debug reached the end of file
+						}
+						// log cannot read csv-row
+						readRow = false
+						break
+					}
+					// fmt.Printf("%v", row)
+					if !readRow {
+						break
+					}
+					// check condition
+					result, err := qs.checker.Check(head, row)
+					if err != nil {
+						fmt.Printf("error checking condition, program exit %s", err)
+						qs.csv.Close()
+						return
+					}
+					if result {
+						printData(qs.parser.GetSelect(), head, row, qs.sep)
+					}
+				}
 			}
 		}
 	}
